3697631de430f73261f5434fc4cc34b2939e871e
Chen, Zhenyu <chwch1860@gmail.com>
2026-05-29 15:53:25 -0700

    file update

---
+++ src/main/java/com/cmpt276/cmpt276_assignment1/Cmpt276Assignment1Application.java
@@ -0,0 +1,13 @@
+package com.cmpt276.cmpt276_assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmpt276Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmpt276Assignment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmpt276/cmpt276_assignment1/Cmpt276Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpt276.cmpt276_assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmpt276Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
